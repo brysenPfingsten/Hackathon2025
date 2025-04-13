b54501c2e834f067c69f3e5c27380c329a006ad9
Astanwa <Astanwa@gmail.com>
2025-04-12 22:22:55 -0400

    aboutpage

---
+++ Deepfakes/frontend/src/pages/goon/About.jsx
@@ -0,0 +1,36 @@
+export default function about() {
+  return (
+    <section id="about">
+      <h2>About us</h2>
+      <p>
+       We are a team of 5 people BLAH BLAH 
+      </p>
+      <img src={whatisdeep} alt="What is a Deepfake?" />
+      <h3>Aaron</h3>
+      <p>
+        Aaron is BLAH BLAH
+      </p>
+      <img src={deepcartoon} alt="Deepfakes as cartoons" />
+      <h4>Dimitri</h4>
+      <p>
+        Dimitriis BLAH BLAH
+      </p>
+      <img src={deepcartoon} alt="Deepfakes as cartoons" />
+      <h7>Thorin</h7>
+      <p>
+        Aaron is BLAH BLAH
+      </p>
+      <img src={deepcartoon} alt="Deepfakes as cartoons" />
+      <h5>Collin</h5>
+      <p>
+        Aaron is BLAH BLAH
+      </p>
+      <img src={deepcartoon} alt="Deepfakes as cartoons" />
+      <h6>Bryson</h6>
+      <p>
+         is BLAH BLAH
+      </p>
+      <img src={deepcartoon} alt="Deepfakes as cartoons" />
+    </section>
+  );
+}
